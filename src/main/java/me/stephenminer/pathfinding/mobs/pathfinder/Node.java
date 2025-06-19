03d79763b029b887dcf9b1c7553fe30df6f93c63
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2025-06-18 22:32:39 -0700

    Remove MoveType | Add build/dig targets

--- src/main/java/me/stephenminer/pathfinding/mobs/pathfinder/Node.java
+++ src/main/java/me/stephenminer/pathfinding/mobs/pathfinder/Node.java
@@ -4,27 +4,18 @@ import net.minecraft.core.BlockPos;
 
 /**
  *
- * @param pos the positon of Node in a World
+ * @param pos the positon of Node in a World (move target)
  * @param parent the previous Node in a path
  * @param moveCost the cost to move from the start Node to this Node
  * @param estCost the heuristic cost to travel from
  *                this node to the goal of the path
- * @param action  What kind of action does this node entail
+ * @param buildTargets  What blocks must we place to reach the specified position
+ * @param digTargets What blocks must we place to reach the specified position
  */
-public record Node(BlockPos pos, Node parent, double moveCost, double estCost, MoveType action) {
+public record Node(BlockPos pos, Node parent, double moveCost, double estCost, BlockPos[] buildTargets, BlockPos[] digTargets, char type) {
 
     public double totalCost(){
         return moveCost + estCost;
     }
 
-    public boolean walk(){ return action == MoveType.WALK; }
-    public boolean dig(){ return action == MoveType.DIG; }
-    public boolean build(){ return action == MoveType.BUILD; }
-
-
-    public enum MoveType{
-        WALK,
-        DIG,
-        BUILD
-    }
 }
